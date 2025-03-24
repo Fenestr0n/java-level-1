84269e0e2f7b83b8bb8867bbf6c50c5858b95209
Roman Kozhin <kozhin.rm@mail.ru>
2025-03-24 09:37:07 +0300

    2.1 Переменные

---
+++ src/HelloWorld/Variable.java
@@ -0,0 +1,30 @@
+package HelloWorld;
+
+public class Variable {
+    public static void main(String[] args) {
+        int x = 5;
+        int y = x + 5;
+        System.out.println(y);
+
+        int john = 100;
+        int nick = john * 5;
+        int sum = john - nick;
+        System.out.println(sum);
+
+        int a = 10;
+        int b = 3;
+        int c = a / b;
+        System.out.println(c); // 3
+        int d = a % b;
+        System.out.println(d); // 1
+
+        int days = 10_000;
+        int years = days / 365;
+        int months = (days - (years * 365)) / 30;
+        int leftDays = days - (years * 365) - (months * 30);
+        System.out.println(years);
+        System.out.println(months);
+        System.out.println(leftDays);
+
+    }
+}
