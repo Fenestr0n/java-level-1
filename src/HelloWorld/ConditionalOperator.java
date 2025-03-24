cbb4abec071d0ce853bae1b6b3ab289c369140c0
Roman Kozhin <kozhin.rm@mail.ru>
2025-03-24 11:41:10 +0300

    2.6 Типы данных. Boolean

--- src/HelloWorld/ConditionalOperator.java
+++ src/HelloWorld/ConditionalOperator.java
@@ -2,10 +2,14 @@ package HelloWorld;
 
 public class ConditionalOperator {
     public static void main(String[] args) {
-        int temp = 23;
-        if (temp > 25) {
+        int temp = 30;
+        boolean hot = temp >= 25;
+        boolean cold = temp <= 22;
+        int time = 23;
+        boolean isNight = time > 22 || time < 6;
+        if (hot && !isNight) {
             System.out.println("Кондиционер включен");
-        } else if (temp < 22){
+        } else if (cold){
             System.out.println("Кондиционер выключен");
         } else {
             System.out.println("Кондиционер ничего не делает");
