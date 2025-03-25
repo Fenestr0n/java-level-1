c71f1e8b07b30b8cc185234677696b660258941e
Roman Kozhin <kozhin.rm@mail.ru>
2025-03-25 18:43:27 +0300

    3.14 Наследование часть 3. Абстрактные классы и методы

--- src/oop/inheritance/Main.java
+++ src/oop/inheritance/Main.java
@@ -2,9 +2,14 @@ package oop.inheritance;
 
 public class Main {
     public static void main(String[] args) {
-        Cat cat = new Cat();
-        Lion lion = new Lion();
-        cat.eat();
-        lion.eat();
+//        Cat cat = new Cat();
+//        Lion lion = new Lion();
+//        cat.eat();
+//        lion.eat();
+
+        Rect rect = new Rect(10, 20);
+        Triangle triangle = new Triangle(10, 10,10);
+        rect.showPerimeter();
+        triangle.showPerimeter();
     }
 }
